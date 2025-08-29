0b1b0a43a26dbaf89e98d7b7f632ac23c29aed75
Vinay-Daripelly <daripellyvinay@gmail.com>
2025-08-29 20:36:36 +0530

    Changed Api route

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
   res.status(200).json({ message: 'API is running.' });
 });
 
